c83bda7f79d607c8635c49582e0c8945841f7e5a
Gregory Talotta <gjtalotta@alaska.edu>
2020-10-13 21:02:01 -0800

    passing first test

---
+++ contigsum.hpp
@@ -0,0 +1,9 @@
+#ifndef FILE_CONTIGSUM_HPP_INCLUDED
+#define FILE_CONTIGSUM_HPP_INCLUDED
+
+template <typename RAIter>
+int contigSum(RAIter first, RAIter last){
+    return 0;
+}
+
+#endif //#ifndef FILE_CONTIGSUM_HPP_INCLUDED
