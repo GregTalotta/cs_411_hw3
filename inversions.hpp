1d120bb8b8f0e26b4582797c1911ec02876b6a73
Gregory Talotta <gjtalotta@alaska.edu>
2020-10-14 11:08:59 -0800

    outline

---
+++ inversions.hpp
@@ -0,0 +1,11 @@
+// inversions.hpp
+// Greg Talotta
+// 14 Oct 2020
+//
+// For CS 411 Fall 2020
+// Used in Assignment 3, Exercise B
+
+template<typename RAIter>
+size_t inversions(RAIter first, RAIter last){
+    //modified merge sort
+}
